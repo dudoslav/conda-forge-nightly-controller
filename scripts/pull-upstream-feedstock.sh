4b9e4ba9844766dab476c64e6b72524369dc166d
Dušan Baran <work.dusanbaran@gmail.com>
2024-03-26 15:29:42 +0100

    Update pull-upstream-feedstock.sh

--- scripts/pull-upstream-feedstock.sh
+++ scripts/pull-upstream-feedstock.sh
@@ -3,6 +3,6 @@ set -eux
 
 repo="$1"
 
-git -C "$repo" remote add upstream "git@github.com:conda-forge/$repo.git"
+git -C "$repo" remote add upstream "https://github.com/conda-forge/$repo.git"
 git -C "$repo" pull --quiet upstream main
-git -C "$repo" push --quiet origin main
+# git -C "$repo" push --quiet origin main
